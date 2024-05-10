754583e2b81eeb794701e9646fe6942abc9ebb08
Kwang-min-lee1 <minwinn1@gmail.com>
2024-05-10 14:38:07 +0900

    버튼

---
+++ ch01_layout/src/main/java/com/busanit/ch01_layout/pr01/ButtonActivity.kt
@@ -0,0 +1,16 @@
+package com.busanit.ch01_layout.pr01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.busanit.ch01_layout.R
+
+class ButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_button)
+
+
+
+    }
+}
