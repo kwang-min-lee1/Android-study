8824f9aa178487ccb16161adbdf759235805b543
Kwang-min-lee1 <minwinn1@gmail.com>
2024-05-10 14:40:07 +0900

    입력(EditText)

---
+++ ch01_layout/src/main/java/com/busanit/ch01_layout/pr01/EditTextActivity.kt
@@ -0,0 +1,21 @@
+package com.busanit.ch01_layout.pr01
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit.ch01_layout.R
+
+class EditTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_text)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
