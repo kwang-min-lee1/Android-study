7726c5e0166131f8c63ed9b27956abe8ddc6bda7
Kwang-min-lee1 <minwinn1@gmail.com>
2024-05-27 15:47:54 +0900

    aa2ba7a9(영속성, sharedPreference, 내부저장소, 외부저장소)

--- ch11_persistence/src/main/java/com/busanit/ch11_persistence/c_external/ExternalActivity.kt
+++ ch11_persistence/src/main/java/com/busanit/ch11_persistence/c_external/ExternalActivity.kt
@@ -4,11 +4,10 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.busanit.ch11_persistence.databinding.ActivityInternalBinding
+import com.busanit.ch11_persistence.databinding.ActivityExternalBinding
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileInputStream
@@ -16,37 +15,35 @@ import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStreamReader
-import kotlin.math.E
 
 class ExternalActivity : AppCompatActivity() {
-    lateinit var binding: ActivityInternalBinding
-    private val REQUEST_CODE = 10;
+    lateinit var binding: ActivityExternalBinding
+    private val REQUEST_CODE = 10
 
     // 외부 저장소
-    // 1. 앱 전용 외부 저장소: 앱이 삭제되면 데이터 함께 삭제 (SD카드 등에 저장 가능) (앱 전용 공간)
-    // 2. 공용 외부 저장소: 앱이 삭제되어도 삭제되지 않음 (디바이스 저장소)
-
+    // 1. 앱 전용 외부 저장소 : 앱이 삭제되면 데이터 함께 삭제 (SD카드 등에 저장 가능) (앱 전용 공간)
+    // 2. 공용 외부 저장소 : 앱이 삭제되어도 삭제되지 않음 (디바이스 저장소)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityInternalBinding.inflate(layoutInflater)
+        binding = ActivityExternalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // 외부 저장소
         // 앱의 외부 저장소에 파일 저장, 다른 앱에서도 접근 가능
-        // 사진, 문서, 용량 등의 대용량 데이터를 앱 내부 저장소외 별개의 공간에 저장
+        // 사진, 문서, 용량 등의 대용량 데이터를 앱 내부 저장소 외 별개의 공간에 저장
         // 외부 저장소에 대한 권한이 필요
 
         // 1. AndroidManifest.xml 에 권한을 추가
         // <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
         // <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
 
-        // 권한 부여 확인
-        val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE)
+        val permissions = arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
 
-        val permissionRequest = mutableListOf<String>()
-
+        val permissionRequest = mutableListOf<String>() // 비승인 권한 목록
         // 권한 체크
         for (permission in permissions) {
             if (ContextCompat.checkSelfPermission(
@@ -60,27 +57,28 @@ class ExternalActivity : AppCompatActivity() {
 
         // 권한 요청
         if (permissionRequest.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                this, permissionRequest
-                    .toTypedArray().REQUEST_CODE)
+            ActivityCompat.requestPermissions(this, permissionRequest.toTypedArray(), REQUEST_CODE)
         }
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
-                PackageManager.PERMISSION_GRANTED) {
-            // 권한이 부여되지 않는 경우
+        /*
+        // 권한 부여 확인
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 권한이 부여되지 않은 경우
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(Manifest.permission
-                    .WRITE_EXTERNAL_STORAGE),10
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE
             )
             // 권한 요청
         }
-
+*/
         val fileName = "external_storage.txt"
 
         binding.run {
-            buttonSave.setOnClickListener{
+            buttonSave.setOnClickListener {
                 val inputData = editTextName.text.toString()
                 // 파일을 데이터에 저장하는 함수
                 saveToFile(fileName, inputData)
@@ -97,56 +95,51 @@ class ExternalActivity : AppCompatActivity() {
                 deleteExternalFile(fileName)
                 textView.text = "파일이 삭제되었습니다."
             }
+
         }
     }
 
     // 삭제
     private fun deleteExternalFile(fileName: String) {
-
         try {
             // 공용 저장소 파일 경로
-            val publicPath =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
-
-            // 파일 경로 가져오기
-            val fileStreamPath = getFileStreamPath((fileName))
-            Log.d("my_log", "onCreate: ${fileStreamPath.toString()}")
+            val publicPath =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
 
             // 파일 경로가 존재하면 삭제
-            if (fileStreamPath != null) {
-                val file = File(publicPath,fileName)
+            if (publicPath != null) {
+                val file = File(publicPath, fileName)
                 file.delete()
             }
         } catch (e: FileNotFoundException) {
             e.printStackTrace()
         }
-
-    }
     }
 
     // 읽기
     private fun readFromFile(fileName: String): String? {
         try {
-            // 공용저장소 경료
+            // 공용저장소 경로
             val publicPath =
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
-            val file = File(publicPath, fileName)  // 파일 객체 생성
+            val file = File(publicPath, fileName)       // 파일 객체 생성
 
             // 입력 스트림 열기
-            val fileInPutStream = FileInputStream(file)
-            val isr = InputStreamReader(fileInPutStream)    // 문자열 열기
+            val fileInputStream = FileInputStream(file)
+            val isr = InputStreamReader(fileInputStream)    // 문자열 읽기
             val br = BufferedReader(isr)                    // 버퍼 리더
-            val sb = StringBuilder()                        // 문자열 담을 문자열빌더
-            var text: String?                               // 한줄 읽을 문자열
+            val sb = StringBuilder()        //  문자열 담을 문자열빌더
+            var text: String?               // 한줄 읽을 문자열
             while (br.readLine().also {
                     text = it
                 } != null) {
                 // 파일을 끝까지 한줄씩 읽고,
-                sb.append(text)  // 빌더에 추가
+                sb.append(text) // 빌더에 추가
             }
             // 리소스 정리
             br.close()
             isr.close()
-            fileInPutStream.close()
+            fileInputStream.close()
 
             // 빌더에 담은 문자열 반환
             val readData = sb.toString()
@@ -154,43 +147,40 @@ class ExternalActivity : AppCompatActivity() {
 
         } catch (e: Exception) {
             e.printStackTrace()
+            return null
         }
-
     }
 
-
     // 저장
-    fun saveToFile(fileName: String, data: String) {
+    private fun saveToFile(fileName: String, data: String) {
         // 외부저장소가 쓰기 가능한지 확인
-        if (Environment.getExternalStorageState()==Environment.MEDIA_MOUNTED) {
+        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
 
             // 앱 전용 외부 저장소 파일 경로 지정
-            val externalPath = getExternalFilesDir(null)   // 앱 전용 저장소
-
-            // 공용 외부 저장소
-            val publicPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+            val externalPath = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) // 앱 전용 저장소
+            // 공용 외부저장소
+            val publicPath =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
 
             // 타입
             Environment.DIRECTORY_PICTURES  // 사진 저장
             Environment.DIRECTORY_MOVIES    // 동영상 저장
             Environment.DIRECTORY_DOCUMENTS // 문서 저장
             Environment.DIRECTORY_DOWNLOADS // 다운로드 폴더
+            // ...
 
-            val file = File(publicPath, fileName)  // 외부 경로 파일 객체 생성
+
+            val file = File(publicPath, fileName) // 외부 경로 파일 객체 생성
 
             // 파일 출력 스트림 쓰기
             try {
                 val fos = FileOutputStream(fileName)
                 fos.write(data.toByteArray())
                 fos.close()
-
             } catch (e: IOException) {
-                e.printStackTrace()   // 예외발생시 출력
+                e.printStackTrace()     // 예외 발생시 출력
             }
-
         }
-
-
     }
 
 
@@ -202,13 +192,14 @@ class ExternalActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager
-                    .PERMISSION_GRANTED) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // 권한이 부여되었습니다. (Pass)
             } else {
-                // 권한이 거부되었습니다
+                // 권한 거부되었습니다.
                 binding.textView.text = "외부 저장소 권한이 거부되었습니다."
             }
+
         }
+
     }
 }
