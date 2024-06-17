03de1bd5e7433329ec93a88311b1326e72253374
Kwang-min-lee1 <minwinn1@gmail.com>
2024-06-17 16:28:44 +0900

    챗봇 프로젝트 (메인화면 구성)

--- chatbot/src/main/java/com/busanit/chatbot/FirstActivity.kt
+++ chatbot/src/main/java/com/busanit/chatbot/FirstActivity.kt
@@ -3,6 +3,7 @@ package com.busanit.chatbot
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class FirstActivity : AppCompatActivity() {
@@ -10,10 +11,33 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
 
+
+        val recommendExerciseButton: Button = findViewById(R.id.recommendExerciseButton)
+
+        val foodMenuButton: Button = findViewById(R.id.foodMenuButton)
+
+        val stopwatchButton: Button = findViewById(R.id.stopwatchButton)
+
         val gptButton: Button = findViewById(R.id.gptButton)
-        gptButton.setOnClickListener {
+
+
+
+        recommendExerciseButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            Toast.makeText(this, "추천 운동 버튼 클릭됨", Toast.LENGTH_SHORT).show()
+        }
+
+        foodMenuButton.setOnClickListener {
+            Toast.makeText(this, "식단 버튼 클릭됨", Toast.LENGTH_SHORT).show()
+        }
+
+        stopwatchButton.setOnClickListener {
+            Toast.makeText(this, "스탑워치 버튼 클릭됨", Toast.LENGTH_SHORT).show()
+        }
+
+        gptButton.setOnClickListener {
+            Toast.makeText(this, "GPT 버튼 클릭됨", Toast.LENGTH_SHORT).show()
         }
 
     }
